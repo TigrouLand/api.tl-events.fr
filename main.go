2d57ad6770b416bc2ae84ebbef03e85b4b401a3f
Romitou <romitou@protonmail.com>
2022-08-23 15:24:11 +0200

    :tada: Initial API commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/tigrouland/api/mongo"
+	"github.com/tigrouland/api/routes"
+	"log"
+)
+
+func main() {
+	// connect the mongodb database
+	mongo.Connect()
+
+	// setup web server
+	r := gin.Default()
+
+	r.Use(cors.Default())
+
+	r.GET("/", routes.Main)
+	r.GET("/members", routes.Members)
+	r.GET("/modifiers", routes.Modifiers)
+	r.GET("/games", routes.Games)
+	r.GET("/stats", routes.Stats)
+
+	err := r.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}

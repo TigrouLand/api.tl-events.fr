024c3582f9d4fed5b729d34ab844f8500e6b5204
Romitou <romitou@protonmail.com>
2025-03-02 19:54:36 +0100

    :memo: Adding .env file loading

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/tigrouland/api/mongo"
 	"github.com/tigrouland/api/routes"
 	"log"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found")
+	}
+
 	// connect the mongodb database
 	mongo.Connect()
 
@@ -23,7 +29,7 @@ func main() {
 	r.GET("/games", routes.Games)
 	r.GET("/stats", routes.Stats)
 
-	err := r.Run()
+	err = r.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
